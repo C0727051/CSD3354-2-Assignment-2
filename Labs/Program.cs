4e8f2aafd77477e251c5554f53dc8aacc1b9c75e
visualstudio <visualstudio@230-PC26>
2019-03-07 15:18:10 -0500

    Commit at 3:17pm(lab7)

--- Labs/Program.cs
+++ Labs/Program.cs
@@ -15,8 +15,8 @@ namespace Labs
             static void Main(string[] args)
             {
                 DelegateExercises delegateExercises = new DelegateExercises();
-
                 delegateExercises.Method3();
+            Console.ReadLine();
 
 
             }
@@ -34,7 +34,7 @@ namespace Labs
 
         {
 
-            return intMethod1*2;
+            return intMethod1*4;
 
         }
 
@@ -44,13 +44,13 @@ namespace Labs
 
         {
 
-            return intMethod2*10;
+            return intMethod2*20;
 
         }
         public void Method4(MyDelegate myDelegate)
         {
-            int result = myDelegate(10);
-            Console.WriteLine(result);
+            for (int i = 1; i <= 5; i++)
+            System.Console.WriteLine(myDelegate(i) + " ");
         }
 
 
