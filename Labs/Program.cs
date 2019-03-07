a9dcbe7ae06fd6c9098da9262d6542c71d6f14f5
visualstudio <visualstudio@230-PC26>
2019-03-07 14:19:52 -0500

    Commit at 2:19(lab3)

--- Labs/Program.cs
+++ Labs/Program.cs
@@ -6,12 +6,7 @@ using System.Threading.Tasks;
 
 namespace Labs
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
+
 
     public class DelegateExercises
     {
@@ -19,15 +14,28 @@ namespace Labs
         //Student ID:Jashandeep Singh Dhillon  Student ID:C0725713
         // Assignment2
         // March 7,2019
-        void Method1()
+        public delegate void MyDelegate();
+        void Method1(int i)
         {
             Console.WriteLine("Method1");
             Console.ReadLine();
         }
         public void Method2()
         {
-            MyDelegate myDelegate = new MyDelegate(Method1);
+            MyDelegate myDelegate = new MyDelegate(Method2);
             myDelegate();
         }
     }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DelegateExercises delegateExercises = new DelegateExercises();
+
+            delegateExercises.Method2();
+        }
+
+
+    }
 }
+
